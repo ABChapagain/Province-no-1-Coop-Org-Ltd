157757b5f1149bc71b1f09249c298c0eef5e1e97
Rejens <rejensraya@gmail.com>
2022-11-29 16:56:33 +0545

    added images

--- admin/products.php
+++ admin/products.php
@@ -1,3 +1,71 @@
 <?php include "./includes.php" ?>
 
-<?php include "./pages/includes/footer.php" ?>
+
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Products</h1>
+                        </div>
+
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+
+
+                            <div class="card">
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                    <table id="example1" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 5%;">Sn.</th>
+                                                <th style="width: 20%;">Name</th>
+                                                <th style="width: 60%;">Description</th>
+                                                <th>Image</th>
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Tea </td>
+                                                <td style="overflow: scroll;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis nam distinctio, unde dolor quam tempora id recusandae eius beatae itaque numquam incidunt. Libero, provident ipsum! Illo sit quos delectus illum consequuntur in sint vero doloribus impedit provident soluta animi laudantium, facere neque eius doloremque inventore sequi similique nisi quis nihil., Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt ducimus eaque consequuntur debitis. Commodi ullam rem est quia, dignissimos quo?</td>
+                                                <td> 4</td>
+                                            </tr>
+
+                                        </tbody>
+
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+    </div>
+    <!-- ./wrapper -->
+
+
+
+    <?php include "./pages/includes/footer.php" ?>
