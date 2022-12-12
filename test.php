ca493fbd5a1ec69f4314c13849459794b097a967
Achyut Chapagain <achyutchapagain05@gmail.com>
2022-12-12 17:30:37 +0545

    for the streak

--- test.php
+++ test.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -20,10 +21,12 @@
 
         foreach ($result as $rows) {
         ?>
-            <div class="col-3"><?php echo $rows['department_name'] ?></div>
+        <div class="col-3"><?php echo $rows['department_name'] ?></div>
 
         <?php
         } ?>
+
+
     </div>
 </body>
 
