3f9c5f0d7f161d9a91fa840256582aedd376ffe6
Rejens <rejensraya@gmail.com>
2022-12-17 12:45:37 +0545

    branch ko issue solve vayo

---
+++ admin/notices.php
@@ -0,0 +1,111 @@
+<?php
+include "./includes.php";
+
+$sql = "select * from notices";
+$result = $conn->query($sql);
+$result->fetch_all(MYSQLI_ASSOC);
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Notices</h1>
+                </div>
+
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+
+            <div class="card">
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <div class="add-button"> <a href="<?php url ?>pages/add/notices.php"> <button class="btn btn-primary">add</button></a></div>
+                        <thead>
+                            <tr>
+                                <th>Sn.</th>
+                                <th>Title</th>
+                                <th>Short Description</th>
+                                <th>Images</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 0;
+                            foreach ($result as $rows) :
+                            ?>
+                                <tr>
+                                    <td><?php echo ++$i ?></td>
+                                    <td><?php echo $rows['title'] ?> </td>
+                                    <td><?php
+                                        $description = $rows['short_description'];
+                                        if (strlen($description) > 30) {
+                                            $description = trim(substr($description, 0, 30));
+                                            $description .= ".....";
+                                        }
+                                        echo $description;
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <?php
+                                            $sql = "select * from notice_images where id=" . $rows['id'];
+                                            $image = $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+                                            foreach ($image as $img) :
+                                            ?>
+                                                <a href="<?php echo notice_url . $img['image'] ?>" data-toggle="lightbox" data-title="<?php echo $rows['title'] ?>">
+                                                    <img src="<?php echo notice_url . $img['image'] ?>" width="50px" class="img-fluid mb-2" alt="image" />
+                                                </a>
+                                            <?php
+                                            endforeach;
+                                            ?>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <a href="<?php echo url ?>pages/view/events.php?id=<?php echo $rows['id'] ?>"><button class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="View"><i class="fas fa-eye"></i></button></a>
+                                        <a href="<?php echo url ?>pages/edit/events.php?id=<?php echo $rows['id'] ?>"><button class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-pen-square"></i></button></a>
+                                        <button class="btn btn-danger" data-toggle="tooltip" onclick="deleteEvent(<?php echo $rows['id'] ?>)" data-placement="top" title="Delete"><i class="fas fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            <?php
+                            endforeach;
+                            ?>
+
+                        </tbody>
+
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php include "./pages/includes/footer.php" ?>
+
+
+<?php
+if (isset($_SESSION['notice_deleted'])) {
+    echo $_SESSION['notice_deleted'];
+    if ($_SESSION['notice_deleted'] == "successful") {
+        echo "<script>success('success', 'notice deleted successfully'); </script>";
+    } else {
+        echo "<script>success('error', 'unable to delete notice'); </script>";
+    }
+    unset($_SESSION['notice_deleted']);
+}
+?>
---
+++ admin/pages/add/notices_post.php
@@ -0,0 +1,61 @@
+<?php
+require "../../config/config.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $description = mysqli_real_escape_string($conn, $_POST['description']);
+    $short_description = mysqli_real_escape_string($conn, $_POST['short_description']);
+    $popupdate =  mysqli_real_escape_string($conn, $_POST['popupdate']);
+    $countfiles = count($_FILES['img']['name']);
+
+    $today = date("Y-m-d");
+
+    $popupdate = explode("-", $popupdate);
+    $popup_start = $popupdate[0];
+    $popup_end = $popupdate[1];
+
+
+
+
+    $sql = "select * from notices where title='$title'";
+    $result = $conn->query($sql);
+    $rowcount = mysqli_num_rows($result);
+    if ($rowcount == 0) {
+        $sql = "insert into notices (title,description,published_date,popup_start_date,popup_end_date,short_description) values('$title','$description','$today','$popup_start','$popup_end','$short_description')";
+        if ($conn->query($sql)) {
+            // $featured_img = $_FILES['featured_img']['name'];
+            // $filename =  uniqid() . ".jpg";
+
+            $sql = "select id from notices where title='$title' order by id desc limit 1";
+            $id = $conn->query($sql)->fetch_assoc()['id'];
+
+            // $sql = "insert into notice_images(id,name,featured) values('$id','$filename','1')";
+            // if ($conn->query($sql))
+            //     move_uploaded_file($_FILES['featured_img']['tmp_name'],  event_upload . $filename);
+
+            if (strlen($_FILES['img']['name'][0]) != 0) {
+                for ($i = 0; $i < $countfiles; $i++) {
+                    $filename = $_FILES['img']['name'][$i];
+                    $ext = explode(".", $filename);
+                    $ext = end($ext);
+                    if (in_array($ext, ["jpg", "png", "jpeg", "svg", "webp"])) {
+                        // Upload file
+                        $filename =  uniqid() . ".jpg";
+                        move_uploaded_file($_FILES['img']['tmp_name'][$i],  notice_upload . $filename);
+                        $sql = "insert into notice_images (id,image) values('$id','$filename')";
+                        $conn->query($sql);
+                    } else {
+                        // echo "<script> alert('please upload photos of specified format') </script>";
+                        die("please upload photos of specified fromat");
+                    }
+                }
+            }
+            $_SESSION['notice_added'] = "successful";
+        } else {
+            $_SESSION['notice_added'] = "unsuccessful";
+        }
+    } else {
+        $_SESSION['notice_added'] = "exists";
+    }
+}
+header("Location:notices.php");
