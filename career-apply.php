1a370ad61fd221680eb8d86936eb3a862b6ee4f6
Rejens <rejensraya@gmail.com>
2022-12-26 18:05:57 +0545

    nothing

---
+++ career-apply.php
@@ -0,0 +1,170 @@
+<?php
+require_once('./components/Header.php');
+require_once('./config/db_config.php');
+
+if (isset($_GET['id']) && !is_null($_GET['id']) && $_GET['id'] != '') {
+
+    $vacancy_id = $_GET['id'];
+    $current_date = date('Y-m-d');
+
+    $sql = "SELECT * FROM vacancy WHERE id = $vacancy_id AND termination_date > '$current_date'";
+    $res = $conn->query($sql);
+    $vacancy = $res->fetch_assoc();
+
+    if ($res->num_rows > 0) {
+        $sql = "SELECT * FROM vacancy WHERE termination_date > '$current_date'";
+        $res = $conn->query($sql);
+        $vacancies = $res->fetch_all(MYSQLI_ASSOC);
+
+?>
+
+
+<!-- Breadcrumb Area Start -->
+<div class="breadcrumb-area bg-image-3 ptb-170">
+    <div class="container">
+        <div class="breadcrumb-content text-center">
+            <h3>Apply For Vaccancy</h3>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="careers.php">Career</a> </li>
+                <li><a href="career.php?id=<?php echo $vacancy_id ?>"><?php echo $vacancy['title'] ?></a> </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb Area End -->
+
+<!-- Career Area Start -->
+<div class="career-area pt-100 pb-95">
+    <div class="container">
+        <div class="section-border mb-35">
+            <div class="text-center section-title-wrap">
+                <h3>Apply For</h3>
+                <h2 class="text-green ">
+                    <?php echo $vacancy['title'] ?>
+                </h2>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-8 shadow p-3 rounded">
+                <?php
+
+                        if (isset($_POST['submit']) && isset($_FILES['resume'])) {
+
+                            $name = mysqli_real_escape_string($conn, $_POST['name']);
+                            $email = mysqli_real_escape_string($conn, $_POST['email']);
+                            $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+                            $address = mysqli_real_escape_string($conn, $_POST['address']);
+                            $position = mysqli_real_escape_string($conn, $_POST['apply-for']);
+                            $cover_letter = mysqli_real_escape_string($conn, $_POST['cover-letter']);
+                            $date = date('Y-m-d');
+
+                            // Get file name
+                            $file_name = $_FILES['resume']['name'];
+
+                            // Allowed extensions
+                            $allowed_ext = array('pdf', 'doc', 'docx', 'jpg');
+
+                            // Get file extension
+                            $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+
+                            if (in_array($file_ext, $allowed_ext)) {
+                                // Set unique id with the extension
+
+                                $uniqID =  uniqid() . "." . pathinfo($file_name, PATHINFO_EXTENSION);
+                                $file_tem_loc = $_FILES['resume']['tmp_name'];
+                                $file_store = "uploads/resume/" . $uniqID;
+
+
+                                $sql = "INSERT INTO `job_application` (`vacancy_id`, `name`, `email`, `phone`, `address`, `position`, `resume`, `cover_letter`, `date`) VALUES ('$vacancy_id', '$name', '$email', '$phone', '$address', '$position', '$uniqID', '$cover_letter', '$date')";
+
+                                if ($conn->query($sql)) {
+                                    move_uploaded_file($file_tem_loc, $file_store);
+                                    echo
+                                    '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>Success!</strong> Your application has been submitted successfully.
+                        <img src="assets/img/icons/x-lg.svg" alt="close" />
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+                                    echo "<script>setTimeout(`location.href = 'index.php'`,2000);</script>";
+                                } else {
+                                    echo
+                                    '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong>Error!</strong> Something went wrong. Please try again.
+                        <img src="assets/img/icons/x-lg.svg" alt="close" />
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+                                }
+                            } else {
+                                echo
+                                '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Error!</strong> File type not allowed. Allowed file types: pdf, doc,docx
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                        <img src="assets/img/icons/x-lg.svg" alt="close" />
+                        </button></div>';
+                            }
+                        }
+
+                        ?>
+                <form action="#" method="POST" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label class="mb-2" for="name">Full Name: </label>
+                        <input type="text" name="name" placeholder="Full Name" required />
+                    </div>
+                    <div class="mb-3">
+                        <label class="mb-2" for="email">Email: </label>
+                        <input type="email" name="email" placeholder="Email" required />
+                    </div>
+                    <div class="mb-3">
+                        <label class="mb-2" for="phone">Mobile Number: </label>
+                        <input type="tel" name="phone" placeholder="Mobile Number" required />
+                    </div>
+                    <div class="mb-3">
+                        <label class="mb-2" for="address">Address: </label>
+                        <input type="text" name="address" placeholder="Address" required />
+                    </div>
+                    <div class="mb-3">
+                        <label class="mb-2" for="apply-for">Apply For: </label>
+                        <select name="apply-for" id="apply-for" required>
+                            <option disabled selected>-- Apply For --</option>
+                            <?php foreach ($vacancies as $vacant) : ?>
+                            <option value="<?php echo $vacant['title'] ?>"
+                                <?php echo $vacant['id'] === $vacancy['id'] ? 'selected' : '' ?>>
+                                <?php echo $vacant['title'] ?>
+                            </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="mb-2" for="resume">Upload Resume Here: </label>
+                        <input type="file" name="resume" required>
+                        <span>Files must be less than 1mb. Allowed file types: pdf, doc,docx</span>
+                    </div>
+                    <div class="mb-3">
+                        <label class="mb-2" for="cover-letter">Cover Letter: </label>
+                        <textarea name="cover-letter" id="cover-letter" cols="30" rows="7" required></textarea>
+                    </div>
+                    <div class="mb-3 text-center">
+                        <input style="width: auto;" type="submit" name="submit" value="Apply Now"
+                            class="btn btn-style-2">
+                    </div>
+                </form>
+
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+
+    } else {
+        echo "<script>setTimeout(`location.href = 'index.php'`,0);</script>";
+    }
+}
+require_once('./components/Footer.php');
+
+?>
---
+++ index.php
@@ -0,0 +1,180 @@
+<?php
+require_once('./components/Header.php');
+require_once('./config/db_config.php');
+
+
+?>
+
+
+<!-- Slider Start -->
+<div class="slider-area">
+    <div class="slider-active owl-dot-style owl-carousel">
+        <div class="single-slider ptb-240 bg-img" style="background-image: url(assets/img/slider/slider-2.jpeg)">
+            <div class="container">
+                <div style="height: 13.3rem;" class="slider-content slider-animated-1">
+                    <h1 class="animated">Want to stay <span class="theme-color">healthy</span></h1>
+                    <h1 class="animated">drink <span class="theme-color">Alpine</span>.</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetu adipisicing elit sedeiu tempor inci ut labore et dolore
+                        magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+        <div class="single-slider ptb-240 bg-img" style="background-image: url(assets/img/slider/slider-3.jpg)">
+            <div class="container">
+                <div style="height: 13.3rem;" class="slider-content slider-animated-1">
+                    <h1 class="animated">Want to stay <span class="theme-color">healthy</span></h1>
+                    <h1 class="animated">drink <span class="theme-color">Alpine</span>.</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetu adipisicing elit sedeiu tempor inci ut labore et dolore
+                        magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+        <div class="single-slider ptb-240 bg-img" style="background-image: url(assets/img/slider/slider-4.jpg)">
+            <div class="container">
+                <div style="height: 13.3rem;" class="slider-content slider-animated-1">
+                    <h1 class="animated">Want to stay <span class="theme-color">healthy</span></h1>
+                    <h1 class="animated">drink <span class="theme-color">Alpine</span>.</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetu adipisicing elit sedeiu tempor inci ut labore et dolore
+                        magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- You can add new slider here just copy upper div -->
+    </div>
+</div>
+<!-- Slider End -->
+
+
+<!-- About Us Area Start -->
+<div id="about-us" class="about-us-area pt-100 pb-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-12">
+                <div class="overview-img text-center">
+                    <a href="about-us.php">
+                        <img class="rounded" src="assets/img/slider/slider-5.jpg" alt="About Us" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 mt-3 mt-lg-0 d-flex align-items-center">
+                <div class="overview-content-2">
+                    <h4>Welcome To</h4>
+                    <h2>Province no. 1 Wholesale Consumer Specialized Cooperative Union Ltd</h2>
+                    <p class="peragraph-blog" style="text-align: justify;">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae,
+                        nemo fugiat, incidunt molestiae velit neque autem possimus
+                        pariatur tempora, laudantium veniam omnis similique dicta eum.
+                        Saepe culpa ratione pariatur necessitatibus optio officiis
+                        aspernatur quam odio eos alias, minima veniam, magnam harum
+                        quia. Animi itaque nam doloribus inventore officiis vel veniam
+                        quos repellendus aut quisquam nisi nostrum esse, provident
+                        assumenda possimus repellat sunt minima dignissimos perspiciatis
+                        neque. Quis optio id consectetur!
+                    </p>
+                    <a class="btn py-2 px-4 rounded text-white bg-project" href="about-us.php">Read
+                        More</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- About Us Area End -->
+
+<?php require_once('./components/OurProducts.php') ?>
+
+<?php require_once('./components/Testimonial.php') ?>
+
+
+
+<!-- Message from CEO -->
+<div class="about-us-page-area ptb-100">
+    <div class="container">
+        <div class="product-top-bar section-border mb-55">
+            <div class="section-title-wrap text-center">
+                <h3 class="section-title">Message from CEO</h3>
+            </div>
+        </div>
+        <div class="d-xl-flex gap-4 align-items-center justify-content-center">
+            <div>
+                <p class="peragraph-blog" style="text-align: justify;">
+                    Hello Namaste everyone 🙏
+                    <br>
+                    <br>
+                    Our organization is an umbrella organization of coopratives and it's vision is to
+                    establish sustainable business entity in the community by marketing goods and
+                    services through cooperatives. We encourage early cooperatives and members to become
+                    entrepreneurs.
+                    <br>
+                    <br>
+
+                    Through cooperatives, we are moving forward by bringing the products of cooperatives
+                    or members to various Nepalese markets, as well as minimizing the potential fraud
+                    that may be caused to consumers by middlemen. This is our first organization in
+                    Nepal which will gather the cooperatives and engage them into the business.
+
+                    <br>
+                    <br>
+                    Finally, I would like to express my sincere gratitude to all of you who have
+                    connected with us directly or indirectly.
+
+                    <br>
+                    <br>
+                    Thank you!
+                </p>
+            </div>
+
+            <div>
+                <div class="overview-img text-center">
+                    <img style="object-fit:cover" width="100%" class="rounded" src="assets/img/team/ceo.JPG"
+                        alt="About Us" />
+                </div>
+                <div class="ceo-title">
+                    <h4>Sandip Giri</h4>
+                    <p>CEO</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php require_once('./components/LatestEvents.php') ?>
+
+
+<?php
+
+// get notices from database
+$current_date = date('Y-m-d');
+$sql = "SELECT * FROM `notices` WHERE popup_start_date < '$current_date' AND popup_end_date > '$current_date'";
+$res = mysqli_query($conn, $sql);
+
+$result = $res->fetch_all(MYSQLI_ASSOC);
+?>
+<!-- Modal -->
+<div class="modal fade" id="popupModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog model-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <img src="assets/img/icons/x-lg.svg" alt="close" />
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Here is I love you.....
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn-style" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php require_once('./components/Footer.php') ?>
